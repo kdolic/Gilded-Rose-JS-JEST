4f8ca4f9348c51a80556b4fb28edb24b8c4af720
Kenan Dolic <kdolic@alumni.iastate.edu>
2021-06-09 18:39:33 -0500

    added majority of items tests

--- test/gilded_rose.test.js
+++ test/gilded_rose.test.js
@@ -1,9 +1,26 @@
 const {Shop, Item} = require("../src/gilded_rose");
 
 describe("Gilded Rose", function() {
-  it("should foo", function() {
-    const gildedRose = new Shop([new Item("foo", 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("fixme");
+  describe("Majority of Items", function() {
+    it("should degrade quality of item twice as fast, after sellIn date passed", function() {
+      const gildedRose = new Shop([new Item("+5 Dexterity Vest", 0, 4)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toBe(-1);
+      expect(items[0].quality).toBe(2);
+    });
+
+    it("Quality of item is never negative", function() {
+      const gildedRose = new Shop([new Item("+5 Dexterity Vest", 10, 0)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toBe(9);
+      expect(items[0].quality).toBe(0);
+    });
+    
+    it("Quality of item is never more than 50", function() {
+      const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 5, 48)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toBe(4);
+      expect(items[0].quality).toBe(50);
+    });
   });
 });
