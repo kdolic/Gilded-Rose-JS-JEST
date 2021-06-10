1e1faacd1b8710d01be94cd6d3f69e60269a409e
Kenan Dolic <kdolic@alumni.iastate.edu>
2021-06-09 19:34:40 -0500

    refactored code for quality decrease if name NOT Sulfuras, added Conjured feature

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -35,17 +35,10 @@ class Shop {
           }
         }
       }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
       if (this.items[i].sellIn < 0) {
         if (this.items[i].name != 'Aged Brie') {
           if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1;
-              }
-            }
+            
           } else {
             this.items[i].quality = this.items[i].quality - this.items[i].quality;
           }
@@ -59,6 +52,32 @@ class Shop {
 
     return this.items;
   }
+
+  // Subtract 1 from the sellIn days if: 
+  sellInDecrease(i) {
+    // If item name is not 'Sulfuras, Hand of Ragnaros'
+    this.items[i].name != 'Sulfuras, Hand of Ragnaros' 
+    // Subtract 1 from the sellIn days value
+    ? this.items[i].sellIn = this.items[i].sellIn - 1 
+    // Otherwise empty or undefined
+    : null;
+  }
+
+   // Subtract 1 from the quality of an item if:
+   qualityDecrease(i) {
+    // If the quality is greater than 0 AND If the item isn't 'Sulfuras, Hand of Ragnaros'
+    this.items[i].quality > 0 && this.items[i].name != 'Sulfuras, Hand of Ragnaros' 
+    // Subtract 1 from the quality
+    ? this.items[i].quality = this.items[i].quality - 1
+    // Otherwise empty or undefined
+    : null;
+    // If the quality is greater than 0 AND If the item isn't 'Conjured Mana Cake'
+    this.items[i].quality > 0 && this.items[i].name == 'Conjured Mana Cake' 
+    // Subtract 1 from the quality
+    ? this.items[i].quality = this.items[i].quality - 1
+    // Otherwise empty or undefined
+    : null;
+ }
 }
 
 module.exports = {
